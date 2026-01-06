8b7b576eef442321b11552df658782862b8fda2c
Karan Pratap Singh <144534418+karandeol-26@users.noreply.github.com>
2026-01-05 21:48:11 -0500

    Change password storage to JSON format
    Refactor password storage to use JSON format instead of plain text. Update save and search functions to handle JSON data structure.

--- password_manager_GUI/main.py
+++ password_manager_GUI/main.py
@@ -16,9 +16,11 @@ and practical usability.
 """
 
 from tkinter import *
+from tkinter import messagebox
 from combinations import create_password
+import json
 
-FILEPATH = "password.txt"
+FILEPATH = "data.json"
 
 window = Tk()
 window.title("Password Manager")
@@ -40,27 +42,71 @@ def save_password():
     website = website_entry.get()
     username = username_entry.get()
     password = password_entry.get()
-    with open(FILEPATH, 'a') as f:
-        f.write(f"Website: {website} | username: {username} | password: {password}\n")
+
+    if len(username) > 0 and len(website) > 0:
+        new_entry = {
+            website: {
+                "username": username,
+                "password": password
+            }
+        }
+        try:
+            # Load existing data
+            with open(FILEPATH, 'r') as file:
+                data = json.load(file)
+            data.update(new_entry)
+            with open(FILEPATH, 'w') as file:
+                json.dump(data, file, indent= 4)
+        except FileNotFoundError:
+            data = {}
+            data.update(new_entry)
+            with open(FILEPATH, 'w') as file:
+                json.dump(data, file, indent= 4)
+    else:
+       messagebox.showinfo(title="Oops", message="Please make sure you haven't left any fields empty.")
+
+
         
+def search_password():
+    website = website_entry.get()
+    if len(website) > 0:
+        try:
+            with open(FILEPATH, 'r') as file:
+                data = json.load(file)
+            searched = data.get(website_entry.get())
+            password = searched.get("password")
+            userid = searched.get("username")
+
+            password_entry.insert(0, password) 
+            username_entry.insert(0, userid)   
+
+        except FileNotFoundError:
+            messagebox.showinfo(title="Oops", message="you haven't saved any passwoords")
+
+        except KeyError:
+            messagebox.showinfo(title="Oops", message=f"you haven't saved a password for{website}")
+    else:
+         messagebox.showinfo(title="Oops", message="Please make sure you haven't left any fields empty.")
 
 
 Label(text="Website:").grid(row=1, column=0)
 Label(text="Email / Username:").grid(row=2, column=0)
 Label(text="Password:").grid(row=3, column=0)
 
-website_entry = Entry(width=35)
-website_entry.grid(row=1, column=1, columnspan=2)
+website_entry = Entry(width=21)
+website_entry.grid(row=1, column=1)
 website_entry.focus()
 
-username_entry = Entry(width=35)
-username_entry.grid(row=2, column=1, columnspan=2)
+username_entry = Entry(width=21)
+username_entry.grid(row=2, column=1, columnspan=1)
 
 password_entry = Entry(width=21)
 password_entry.grid(row=3, column=1)
 
 
+Button(text="Search Password", command=search_password).grid(row=1, column=2)
 Button(text="Generate Password", command=generate_password).grid(row=3, column=2)
 Button(text="Save", width=36, command=save_password).grid(row=4, column=1, columnspan=2)
 
 window.mainloop()
+
